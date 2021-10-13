871dae72c45f3a48c46d8c6cacfd623f5f0bf85c
Ashish Gupta <ashishgupta8051@gmail.com>
2021-10-13 15:24:43 +0530

    commit successful

--- app/src/main/java/com/connect/androidinternassignment3/adapter/BestSellersAdapter.kt
+++ app/src/main/java/com/connect/androidinternassignment3/adapter/BestSellersAdapter.kt
@@ -2,8 +2,12 @@ package com.connect.androidinternassignment3.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import com.connect.androidinternassignment3.R
 import com.connect.androidinternassignment3.databinding.BestSellersListBinding
 import com.connect.androidinternassignment3.model.BestSellers
 
@@ -15,9 +19,15 @@ class BestSellersAdapter : RecyclerView.Adapter<BestSellersAdapter.BestSellersHo
         val binding:BestSellersListBinding = binding
     }
 
+   /* inner class BestSellersHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var binding = DataBindingUtil.bind(itemView)
+    }*/
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BestSellersHolder {
         val binding = BestSellersListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return BestSellersHolder(binding)
+      /*  val view: View = LayoutInflater.from(parent.context).inflate(R.layout.best_sellers_list, parent, false)
+        return BestSellersHolder(view)*/
     }
 
     override fun onBindViewHolder(holder: BestSellersHolder, position: Int) {
